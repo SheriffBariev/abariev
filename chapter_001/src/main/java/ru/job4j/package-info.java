1e7a75edab883542da1d61ac5fabc06cdd5d4e0f
SheriffBariev <abdullaiman06@gmail.com>
2017-09-20 21:03:10 +0300

    1.3. JavaDoc. Code convention.[#4087]

---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,16 @@
+/**
+*Package for calculate task.
+*
+*@author Ayup Bariev (mailto:abdullaiman06@gmai.com)
+*@version 1.3
+*@since 20.09.2017
+*/
+package ru.job4j;
+
+public class Calculate{
+	
+	public static void main(String[] args){
+	System.out.println("Hello world.");
+	}
+
+}
